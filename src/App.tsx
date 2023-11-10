b56c15e5da40607b6b826c4b419a973abc4a3a5a
sudosf <sudosprogrammer@gmail.com>
2023-11-10 03:02:03 +0200

    project reformatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,142 @@
-import { Button } from "react-bootstrap";
-import "./App.css";
+import {
+    AiFillTwitterCircle,
+    AiFillLinkedin,
+    AiFillYoutube,
+} from "react-icons/ai";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useState } from "react";
+import portfolio from "../public/portfolio_2.png";
+import code from "../public/code.png";
+import design from "../public/design.png";
+import consulting from "../public/consulting.png";
 
-/** Environment variables */
-// import.meta.env.PORT
+const App: React.FC = () => {
+    const [darkMode, setDarkMode] = useState<boolean>(true);
 
-function App() {
     return (
-        <>
-            <h1 className="text-5xl font-bold underline">Hello world!</h1>
-            <Button variant="warning"> Hello Bootstrap </Button>
-        </>
+        <div className={darkMode ? "dark" : ""}>
+            <main className="bg-slate-50 dark:bg-gray-900 px-10  md:px-20 lg:px-40">
+                <section className=" dark:bg-gray-900">
+                    <nav className="py-10 mb-12 flex justify-between dark:text-white">
+                        <h1 className="font-burtons text-xl">developedbyed</h1>
+                        <ul className="flex items-center">
+                            <li>
+                                <BsFillMoonStarsFill
+                                    onClick={() => setDarkMode(!darkMode)}
+                                    className="cursor-pointer text-2xl"
+                                />
+                            </li>
+                            <li>
+                                <a
+                                    className="no-underline bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                                    href="#"
+                                >
+                                    Resume
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="text-center p-10 py-10">
+                        <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+                            Dimitri Marco
+                        </h2>
+                        <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+                            Developer and designer.
+                        </h3>
+                        <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+                            Freelancer providing services for programming and
+                            design content needs. Join me down below and let's
+                            get cracking!
+                        </p>
+                        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+                            <AiFillTwitterCircle />
+                            <AiFillLinkedin />
+                            <AiFillYoutube />
+                        </div>
+                        <div className="mx-auto bg-gradient-to-b from-slate-400 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+                            <img
+                                src={portfolio}
+                                className="object-cover h-full w-full"
+                            />
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h3 className="text-3xl py-1 dark:text-white ">
+                            Services I offer
+                        </h3>
+                        <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+                            Since the beginning of my journey as a freelance
+                            designer and developer, I've done remote work for
+                            <span className="text-teal-500"> agencies </span>
+                            consulted for{" "}
+                            <span className="text-teal-500">startups </span>
+                            and collaborated with talanted people to create
+                            digital products for both business and consumer use.
+                        </p>
+                        <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+                            I offer from a wide range of services, including
+                            brand design, programming and teaching.
+                        </p>
+                    </div>
+                    <div className="lg:flex gap-10">
+                        <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+                            <img src={design} className="mx-auto" />
+                            <h3 className="text-lg font-medium pt-8 pb-2  ">
+                                Beautiful Designs
+                            </h3>
+                            <p className="py-2">
+                                Creating elegant designs suited for your needs
+                                following core design theory.
+                            </p>
+                            <h4 className="py-4 text-teal-600">
+                                Design Tools I Use
+                            </h4>
+                            <p className="text-gray-800 py-1">Photoshop</p>
+                            <p className="text-gray-800 py-1">Illustrator</p>
+                            <p className="text-gray-800 py-1">Figma</p>
+                            <p className="text-gray-800 py-1">Indesign</p>
+                        </div>
+                        <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+                            <img src={code} className="mx-auto" />
+                            <h3 className="text-lg font-medium pt-8 pb-2 ">
+                                Code your dream project
+                            </h3>
+                            <p className="py-2">
+                                Do you have an idea for your next great website?
+                                Let's make it a reality.
+                            </p>
+                            <h4 className="py-4 text-teal-600">
+                                Design Tools I Use
+                            </h4>
+                            <p className="text-gray-800 py-1">Photoshop</p>
+                            <p className="text-gray-800 py-1">Illustrator</p>
+                            <p className="text-gray-800 py-1">Figma</p>
+                            <p className="text-gray-800 py-1">Indesign</p>
+                        </div>
+                        <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+                            <img src={consulting} className="mx-auto" />
+                            <h3 className="text-lg font-medium pt-8 pb-2 ">
+                                Consulting
+                            </h3>
+                            <p className="py-2">
+                                Are you interested in feedback for your current
+                                project? I can give you tips and tricks to level
+                                it up.
+                            </p>
+                            <h4 className="py-4 text-teal-600">
+                                Design Tools I Use
+                            </h4>
+                            <p className="text-gray-800 py-1">Photoshop</p>
+                            <p className="text-gray-800 py-1">Illustrator</p>
+                            <p className="text-gray-800 py-1">Figma</p>
+                            <p className="text-gray-800 py-1">Indesign</p>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  darkMode: "class",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                burtons: "burtons",
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
